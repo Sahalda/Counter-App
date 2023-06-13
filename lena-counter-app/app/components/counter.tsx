bc9b33de0806a5d80f40ea95966d1b1df13a786c
Sahalda <136367486+Sahalda@users.noreply.github.com>
2023-06-13 11:38:45 -0500

    Update counter.tsx

--- lena-counter-app/app/components/counter.tsx
+++ lena-counter-app/app/components/counter.tsx
@@ -6,15 +6,21 @@ const counter = () => {
     const incrementCounter = () => {
         setCount(count + 1);
            
-}
+};
+    const decrementcounter = () => {
+    setCount(count - 1);
+};
+const reset = () =>{
+    setCount(0)
+};
     return (
        
 <div>
     <h1 className = "componentHeading">Counter</h1>
     <p className = "countLabel">Count: {count}</p>
-    <button className = "Fancy Button" onClick={()=>incrementCounter()}>increment</button>
-    <button className="Fancy Button">Decrement</button>
-    <button className="Fancy Button">Reset</button>
+    <button className = "fancybutton" onClick={()=>incrementCounter()}>increment</button>
+    <button className="fancybutton" onClick={()=>decrementcounter()}>>Decrement</button>
+    <button className="fancybutton" onClick={()=>reset()}>Reset</button>
 </div>
     )
 }
