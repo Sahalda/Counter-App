4e3aba9475d823f296b786707a8f8a0ee48b63b6
Lena <lenapringlr8@gmail.com>
2023-06-14 11:48:04 -0500

    Fixing mistakes

--- lena-counter-app/app/components/counter.tsx
+++ lena-counter-app/app/components/counter.tsx
@@ -1,43 +1,41 @@
 import { useState } from "react"
 
 
-const counter = () => {
-    const[count, setCount] = useState(0)
+const Counter = () => {
+    const [count, setCount] = useState(0);
     const incrementCounter = () => {
         setCount(count + 1);
-           
-};
+
+    };
 
 
-const decrementcounter = () => {
-    setCount(count - 1);
-};
-const reset = () =>{
-    setCount(0)
-};
+    const decrementcounter = () => {
+        setCount(count - 1);
+    };
+    const reset = () => {
+        setCount(0)
+    };
 
-const countClass = () => {
-    if (count > 0)
-    {
-        return "greater-than-zero";
+    const countClass = () => {
+        if (count > 0) {
+            return "greater-than-zero";
+        }
+        else {
+            return "less-than-zero";
+        }
     }
-    else
-    {
-        return "less-than-zero";
-    }
-}
     return (
-       
-<div>
-    <h1 className = "componentHeading">Counter</h1>
-    <p className = {countClass()}>Count: {count}</p>
-    <button className = "fancybutton" onClick={()=>incrementCounter()}>Increment</button>
-    <button className="fancybutton" onClick={()=>decrementcounter()}>Decrement</button>
-    <button className="fancybutton" onClick={()=>reset()}>Reset</button>
-</div>
+
+        <div>
+            <h1 className="componentHeading">Counter</h1>
+            <p className={countClass()}>Count: {count}</p>
+            <button className="fancybutton" onClick={() => incrementCounter()}>Increment</button>
+            <button className="fancybutton" onClick={() => decrementcounter()}>Decrement</button>
+            <button className="fancybutton" onClick={() => reset()}>Reset</button>
+        </div>
     )
 }
 
 
 
-export default counter;
+export default Counter;
